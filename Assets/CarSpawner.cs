ba31b9354e5e516fcad8d26f2c7d04bdfd1a7ae3
Walter Adrian Samuel <walter.adrian@student.umn.ac.id>
2024-12-07 01:57:56 +0700

    Terrain and objects

---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject carPrefab; 
+    public Transform[] spawnPoints; 
+    public float spawnInterval = 2f;
+    public float carSpeed = 5f; 
+
+    void Start()
+    {
+        StartCoroutine(SpawnCars());
+    }
+
+    IEnumerator SpawnCars()
+    {
+        while (true)
+        {
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+            GameObject car = Instantiate(carPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            car.AddComponent<CarMover>().speed = carSpeed;
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
